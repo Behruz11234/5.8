40ae6e25e15a5831996d2ab64937be5e9a099a24
Behruz11234 <komilovbehruz5500@gmail.com>
2026-06-08 14:26:34 +0500

    init

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db.config");
-
-const authRouter = require("./router/auth.routes");
 const authorRouter = require("./router/author.routes");
 const bookRouter = require("./router/book.routes");
-
 const errorMiddleware = require("./middleware/error.middleware");
+const cookieParser = require("cookie-parser");
+const CitationRouter = require("./router/citation.routes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(cookieParser());
 connectDB();
 
-app.use("/api/auth", authRouter);
-app.use("/api/authors", authorRouter);
-app.use("/api/books", bookRouter);
-
+app.use(authorRouter);
+app.use(bookRouter);
+app.use(CitationRouter);
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server is running at: " + PORT);
 });
